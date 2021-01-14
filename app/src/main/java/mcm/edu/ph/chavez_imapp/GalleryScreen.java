cbc7978b9c3a3bddb347dd3e2fedaecef8f94140
seanchvz <seann.chvz@gmail.com>
2021-01-14 23:02:11 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/chavez_imapp/GalleryScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.chavez_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GalleryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_screen);
+    }
+}
